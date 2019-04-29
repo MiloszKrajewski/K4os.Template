d8bb0ed86da4735038c721a512d67601e6225788
Milosz Krajewski <miloszkrajewski@o2.pl>
2019-04-29 20:24:59 +0100

    test verbosity

--- content/build.tools.fsx
+++ content/build.tools.fsx
@@ -198,6 +198,7 @@ module Proj =
                 NoBuild = true
                 NoRestore = true
                 Configuration = DotNet.Release
+                Common = { p.Common with Verbosity = Some DotNet.Normal }
             })
     let testMany projects =
         projects |> Seq.iter test
